a44452076392bdadae2b679e08229c64e7ed1484
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2024-10-27 11:47:52 -0300

    Criando as TabViews

--- main.py
+++ main.py
@@ -10,10 +10,28 @@ janela.geometry("+0+0")
 janela.resizable(width=False, height=False)
 janela.attributes('-topmost', True)
 
-#componente frame para executar as funções dos buttons
-frame1 = ctk.CTkFrame(master=janela, width=1100, height=690).place(x=250, y=0)
+#componente frame para executar as funções do tab view
+#frame1 = ctk.CTkFrame(master=janela, width=1100, height=690, fg_color="white").place(x=250, y=0)
+#frame2 = ctk.CTkFrame(janela, width=250, height=690).place(x=0, y=0)
 
-frame2 = ctk.CTkFrame(janela, width=200, height=690).place(x=0, y=0)
+
+
+#Abas
+tabview = ctk.CTkTabview(janela, width=1000, height=650)
+tabview.pack()
+tabview.add("Filtros")
+tabview.add("Negativo")
+tabview.add("Histograma")
+tabview.add("Morfologia")
+tabview.tab("Filtros").grid_columnconfigure(0, weight=1)
+tabview.tab("Negativo").grid_columnconfigure(0, weight=1)
+tabview.tab("Histograma").grid_columnconfigure(0, weight=1)
+tabview.tab("Morfologia").grid_columnconfigure(0, weight=1)
+
+
+#Adicionando Elementos na Tab
+
+
 
 # Inicia o loop da interface
 janela.mainloop()
